29b3424723c1dbb22a9a571298923bfb0f2d3b66
Antinegr <artyomhovhannisyan.ait@gmail.com>
2021-07-20 11:10:10 +0400

    change inputs

--- src/pages/Studio/StudioContact.jsx
+++ src/pages/Studio/StudioContact.jsx
@@ -7,17 +7,21 @@ import Modal from "../../components/Modal/Modal";
 
 export default function StudioContact() {
   const [openModal, setOpenModal] = useState(false);
+  const [address, setAddress] = useState(``);
+  const [phoneNumber, setPhoneNumber] = useState(``);
+  const [emailAddress, setEmailAddress] = useState(``);
+
   const pageInfo = {
     pageHeader: "Studio",
     pageName: "Contact",
     className: "studio-layout-info menu-left-bar",
   };
-  const contacts = ["Adress:", "Phone Number:", "E-mail address:"];
+
   return (
     <div className="studio-contact">
       {openModal && (
         <Portal node={document.body}>
-          <Modal setOpenModal={setOpenModal} type = {"contact image"}/>
+          <Modal setOpenModal={setOpenModal} type={"contact image"} />
         </Portal>
       )}
       <MainLayout pageInfo={pageInfo}>
@@ -43,21 +47,54 @@ export default function StudioContact() {
                 />
               </div>
               <div className="studio-contact-page-info">
-                {contacts.map((el, i) => {
-                  return (
-                    <div className="contact-input-div" key={i + "a"}>
-                      <p key={i + "b"}>{el}</p>
-                      <div className="contact-input">
-                        <input type="text" key={i + "c"} />
-                        <img
-                          src="../../images/trash.png"
-                          className="trash-icon"
-                          alt="trash"
-                        />
-                      </div>
-                    </div>
-                  );
-                })}
+                <div className="contact-input-div">
+                  <p>Address:</p>
+                  <div className="contact-input">
+                    <input
+                      type="text"
+                      value={address}
+                      onInput={(e) => setAddress(e.target.value)}
+                    />
+                    <img
+                      src="../../images/trash.png"
+                      className="trash-icon"
+                      alt="trash"
+                      onClick={() => setAddress(``)}
+                    />
+                  </div>
+                </div>
+                <div className="contact-input-div">
+                  <p>Phone Number:</p>
+                  <div className="contact-input">
+                    <input
+                      type="text"
+                      value={phoneNumber}
+                      onInput={(e) => setPhoneNumber(e.target.value)}
+                    />
+                    <img
+                      src="../../images/trash.png"
+                      className="trash-icon"
+                      alt="trash"
+                      onClick={() => setPhoneNumber(``)}
+                    />
+                  </div>
+                </div>
+                <div className="contact-input-div">
+                  <p>E-mail address:</p>
+                  <div className="contact-input">
+                    <input
+                      type="text"
+                      value={emailAddress}
+                      onInput={(e) => setEmailAddress(e.target.value)}
+                    />
+                    <img
+                      src="../../images/trash.png"
+                      className="trash-icon"
+                      alt="trash"
+                      onClick={() => setEmailAddress(``)}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
