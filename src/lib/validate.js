24f34503e55a126f7538907b7ac93d9c78d0c78e
Antinegr <artyomhovhannisyan.ait@gmail.com>
2021-07-20 11:11:12 +0400

    log in validation

--- src/lib/validate.js
+++ src/lib/validate.js
@@ -1,7 +1,29 @@
-// import Joi from "@hapi/joi";
-// export function validation(FormData, type = undefined) {
-//   const schema = {
-//     email: Joi.string().min(6).required().email(),
-//     password: Joi.string().min(8).required(),
-//   };
-// }
+import Joi from "@hapi/joi";
+
+export function loginValidation(data) {
+  const schema = {
+    email: Joi.string()
+      .required()
+      .email({ tlds: { allow: false } }),
+    password: Joi.string().min(8).required(),
+  };
+
+  return Joi.object(schema).validate(data);
+}
+export function resetPasswordEmailValidation(data) {
+  const schema = {
+    email: Joi.string()
+      .required()
+      .email({ tlds: { allow: false } }),
+  };
+
+  return Joi.object(schema).validate(data);
+}
+export function resetPasswordValidation(data) {
+  const schema = {
+    password: Joi.string().min(8).required(),
+    repeatPassword: Joi.string().min(8).required(),
+  };
+
+  return Joi.object(schema).validate(data);
+}
